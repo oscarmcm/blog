6df44993e3f6a0a8aa50a0783faf63e52e6ebf2f
Oscar Cortez <om.cortez.2010@gmail.com>
2015-10-05 02:59:48 -0600

    fix with deploy and seo

--- Gruntfile.js
+++ Gruntfile.js
@@ -283,7 +283,7 @@ module.exports = function (grunt) {
     buildcontrol: {
       dist: {
         options: {
-          remote: '../',
+          remote: 'git@github.com:oscarmcm/oscarmcm.com-blog.git',
           branch: 'gh-pages',
           commit: true,
           push: true
